63bda026ceeb1fc4cf6c7ad1367163f5b6843861
Tahaberk <berksysl.g@gmail.com>
2022-11-23 15:46:02 +0300

    Counter start first version

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/*DOM ELEMENTS*/
 const clockTime = document.getElementById("clock-time");
 
+/* Getting Current Time and showing in the DOM */
 function ShowTime(){
     var time = new Date();
     var hours = time.getHours();
@@ -13,4 +15,38 @@ function ShowTime(){
 }
 
 setInterval(ShowTime, 1000);
+
+/* Timer */
+function startTimer(){
+    const remainedMinutes = document.getElementById("minutes");
+    const remainedSeconds = document.getElementById("seconds");
+
+    //Converting current text to int
+    let min = parseInt(remainedMinutes.textContent, 10);
+    let sec = parseInt(remainedSeconds.textContent, 10);
+    
+    if(sec === 0){
+        min--;
+        sec = 59
+    }
+    else{
+        sec--;
+    }
+    updateTime(remainedMinutes, remainedSeconds, min, sec);
+}
+
+function updateTime(remainedMinutes, remainedSeconds, min, sec){
+    if(min < 10){
+        min = "0" + min;
+    }
+    if(sec < 10){
+        sec = "0" + sec;
+    }
+    remainedMinutes.textContent = min;
+    remainedSeconds.textContent = sec;
+}
+
+function start(){
+    setInterval(startTimer, 1000);
+}
 
